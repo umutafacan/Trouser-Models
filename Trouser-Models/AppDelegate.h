9c364658b6f65e228593ded8c9079d64a6ab34a2
Umut Afacan <afacan.umut@mobikasaba.com>
2015-07-01 22:59:52 +0300

    Initial Commit

---
+++ Trouser-Models/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trouser-Models
+//
+//  Created by umut on 01/07/15.
+//  Copyright (c) 2015 Umut Afacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
